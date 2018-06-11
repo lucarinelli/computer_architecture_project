b4914672549288f3605d46fe45db8f66491d80ba
Luca Rinelli <rinelliluca@gmail.com>
2018-06-11 13:45:04 +0200

    test_bram working

---
+++ test_bram/test_bram.cache/ip/2018.1/9a02f1f2d7390a15/clk_wiz_0_stub.v
@@ -0,0 +1,22 @@
+// Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2018.1 (win64) Build 2188600 Wed Apr  4 18:40:38 MDT 2018
+// Date        : Mon Jun 11 10:51:32 2018
+// Host        : DESKTOP-0KG3I12 running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+//               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ clk_wiz_0_stub.v
+// Design      : clk_wiz_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix(memclk, reset, locked, sysclk)
+/* synthesis syn_black_box black_box_pad_pin="memclk,reset,locked,sysclk" */;
+  output memclk;
+  input reset;
+  output locked;
+  input sysclk;
+endmodule
